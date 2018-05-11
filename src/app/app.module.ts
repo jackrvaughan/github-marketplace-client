1a38cd41bd1f939306006cf4510feeba2e4f1c83
jackrvaughan <jackson.vaughan@stern.nyu.edu>
2018-05-11 10:43:00 -0400

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './shared/nav/nav.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'marketplace',
+    component: MarketplaceComponent
+  },
+  // { path: 'app/:id', component: ShowcaseComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavComponent
+    NavComponent,
+    MarketplaceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
