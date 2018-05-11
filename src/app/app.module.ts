989e06c8e36b4be5d634199c42b549dc1b1a07c3
jackrvaughan <jackson.vaughan@stern.nyu.edu>
2018-05-11 12:08:39 -0400

    added apollo angular and rxjs 6 backward compatability

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
+// third party modules
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
+
+// components
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './shared/nav/nav.component';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
+
+// services
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -34,11 +43,41 @@ const appRoutes: Routes = [
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
-    )
+    ),
+    HttpClientModule, // provides HttpClient for HttpLink
+    ApolloModule,
+    HttpLinkModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    apollo: Apollo,
+    httpLink: HttpLink
+  ) {
+    const http = httpLink.create({uri: 'https://api.github.com/graphql'});
+    const auth = setContext((_, { headers }) => {
+      // get the authentication token from local storage if it exists
+      // const token = localStorage.getItem('token');
+      const token = '571e78406e08a4c0866c686a1862df729b195d83';
+      // return the headers to the context so httpLink can read them
+      // in this example we assume headers property exists
+      // and it is an instance of HttpHeaders
+      if (!token) {
+        return {};
+      } else {
+        return {
+          headers: headers.append('Authorization', `Bearer ${token}`)
+        };
+      }
+    });
+
+    apollo.create({
+      link: auth.concat(http),
+      cache: new InMemoryCache()
+    });
+  }
+}
 
 
--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+
+import { Subscription } from 'rxjs';
+
+// const CurrentUserForProfile = gql`
+//   query {
+//     viewer {
+//       login
+//     }
+//   }
+// `;
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  // private querySubscription: Subscription;
+
+  constructor(apollo: Apollo) {
+    // apollo.query({query: gql`{ hello }`}).subscribe(console.log);
+  }
 }
